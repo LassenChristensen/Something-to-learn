27c69dd0c42bb7f84f3026521a2190908ca100eb
LassenChristensen <lass005f@mail.hansenberg.dk>
2023-12-06 08:34:02 +0100

    I made a calculator

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Mit navn er sten");
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Velkommen til din lommeregner");
+        float pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286f;
+        Console.WriteLine("Indtast radius");
+        string radius = Console.ReadLine();
+        float radius1 = float.Parse(radius);
+        float areal = pi * radius1 * radius1;
+        Console.WriteLine("Arealet er " + areal);
+        Console.WriteLine("Indtast højde");
+        string højde = Console.ReadLine();
+        float højde1 = float.Parse(højde);
+        float rumfang = areal * højde1;
+        Console.WriteLine("Rumfanget er " + rumfang);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
